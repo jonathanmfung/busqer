35f76dd722c1e47f0d5e174c01b681d1e72d774c
Jonathan Fung <jonathanfung2000@gmail.com>
2025-05-02 16:13:09 -0700

    pkgs: nixify obus and lwt_glib

--- shell.nix
+++ shell.nix
@@ -4,36 +4,72 @@ let
       # f9f0d5 is 241203
       (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f9f0d5c5380be0a599b1fb54641fa99af8281539.tar.gz")
       { };
+  obus = pkgs.ocamlPackages.buildDunePackage {
+    pname = "obus";
+    version = "1.2.5";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "ocaml-community";
+      repo = "obus";
+      rev = "6d9470dbc22b607ea4eadc734ea9b547d8bf5c51";
+      hash = "sha256-0nq41c35IICCQ8dJhjuWoIyaBms+/KOwIqumqtCXP7g=";
+    };
+
+    nativeBuildInputs = [
+      pkgs.ocamlPackages.menhir
+    ];
+    buildInputs = with pkgs.ocamlPackages; [
+      lwt_ppx
+      lwt_log
+      xmlm
+      lwt_react
+      menhirLib
+    ];
+  };
+  lwt_glib = pkgs.ocamlPackages.buildDunePackage {
+    pname = "lwt_glib";
+    version = "1.1.1";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "ocsigen";
+      repo = "lwt_glib";
+      rev = "72e67ca04cac0219bdb891b17d27d089d02551e1";
+      hash = "sha256-qZ3vqqwvDL6Xe65Wqqj2/qg+Ia44Sr26SF6siMtf0fE=";
+    };
+
+    nativeBuildInputs = [ pkgs.pkg-config ];
+    buildInputs = [
+      pkgs.ocamlPackages.lwt
+      pkgs.glib
+    ];
+    doCheck = true;
+  };
 in
 pkgs.mkShell {
-  pname = "water_sort";
-  src = ./.;
-  buildInputs = with pkgs; [
-    pkg-config
-
-    # GTK
-    glib
-    gtk2
-
-    # TLS for Conduit/Cohttp
-    ocamlPackages.lwt_ssl
-
-    # OUnit
-    ocamlPackages.ounit
-  ];
+  pname = "spotify_dbus";
+  # src = ./.;
   packages = with pkgs; [
-    opam
+    pkg-config
     ocamlPackages.ocaml
     ocamlPackages.dune_3
-    # ocamlPackages.findlib
-    ocamlPackages.utop
-    # ocamlPackages.odoc
-    ocamlPackages.ocaml-lsp
-    ocamlformat
     ocamlPackages.lablgtk # Version 2.18
+
+    # Direct Deps
     ocamlPackages.cohttp-lwt-unix
+    obus
+    lwt_glib
+
+    # Indirect Deps
+    ocamlPackages.lwt_ssl # TLS for Conduit/Cohttp
+    ocamlPackages.ounit
+    ocamlPackages.xmlm
 
     # for scripts/
     graphviz
+
+    # dev
+    ocamlPackages.utop
+    ocamlPackages.ocaml-lsp
+    ocamlformat
   ];
 }
