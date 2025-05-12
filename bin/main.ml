92adad7aafa387c6923e4b9ad65e0edb81731d01
Jonathan Fung <jonathanfung2000@gmail.com>
2025-05-12 16:55:52 -0700

    bin: slightly working album art color extraction
    seems to only run clustering on init, not reacting on further updates to art_url
    
    is also very slow, even with clusters=3, so need to work profile and fix

--- bin/main.ml
+++ bin/main.ml
@@ -36,14 +36,21 @@ let gui () =
      let track_info_w =
        GMisc.label ~text:"Hello, world!" ~packing:vbox#pack ()
      in
+     track_info_w#set_name "track_info";
      let length_w = GMisc.label ~text:"Hello, world!" ~packing:vbox#pack () in
+     length_w#set_name "length";
      let art_url_w = GMisc.label ~text:"Hello, world!" ~packing:vbox#pack () in
+     art_url_w#set_name "art_url";
      let volume_w = GMisc.label ~text:"Hello, world!" ~packing:vbox#pack () in
+     volume_w#set_name "volume";
      let playback_status_w =
        GMisc.label ~text:"Hello, world!" ~packing:vbox#pack ()
      in
+     playback_status_w#set_name "playback_status";
      let position_w = GMisc.label ~text:"Hello, world!" ~packing:vbox#pack () in
+     position_w#set_name "position";
      let rate_w = GMisc.label ~text:"Hello, world!" ~packing:vbox#pack () in
+     rate_w#set_name "rate";
 
      (* Quit when the window is closed. *)
      ignore (window#connect#destroy (Lwt.wakeup_later wakener));
@@ -135,8 +142,87 @@ let gui () =
      in
 
      (* Update Album Art *)
+     let rng_elt arr =
+       let n = Random.int (Array.length arr) in
+       Array.get arr n in
+
+     let mat_to_centroids (m : Lacaml.S.mat) =
+       let tree = OtsuPcaPart.cluster m 3 in
+       OtsuPcaPart.centroids tree
+     in
+
+     let centroids_to_ran_color c =
+       OtsuPcaPart.to_hexstring (rng_elt (Array.of_list c))
+     in
+
+
+     (* let pb_to_ran_color pb : string= *)
+     (*   let data = ArtUrl.(pb_array_to_mat (pixbuf_to_array pb)) in *)
+     (*   Printf.printf "data done\n"; *)
+     (*   mat_to_ran_color data *)
+     (* in *)
+
+     let css_provider_from_data data =
+       let provider = GObj.css_provider () in
+       provider#load_from_data data;
+       provider in
+
+     let mk_provider (css:string) = (css_provider_from_data css)#as_css_provider in
+     let add_provider_to_screen css_prov =
+       GtkData.StyleContext.add_provider_for_screen
+         (Gdk.Screen.default ())
+         css_prov
+         GtkData.StyleContext.ProviderPriority.application in
+     let remove_provider_from_screen css_prov =
+       GtkData.StyleContext.remove_provider_for_screen
+         (Gdk.Screen.default ())
+         css_prov in
+     (* TODO: Making full css string will be horrible because it means all FRP components feed into one big thing,
+        which will probably break the concurrency law
+      *)
+     let css_str color = Format.sprintf "* { color: blue } label#track_info {background-color: %s}" color in
+     (* let red = mk_provider (css_str "red") in *)
+     (* let green = mk_provider (css_str "#00ff00") in *)
+     (* add_provider_to_screen red; *)
+     (* let* () = Lwt_unix.sleep 2.0 in *)
+     (* remove_provider_from_screen red; *)
+     (* add_provider_to_screen green; *)
+
+     (* track_info_w#misc#style_context#add_provider (\* Does not cascade! *\) *)
+     (*   (css_provider_from_data "* { background-color: red }") *)
+     (*   GtkData.StyleContext.ProviderPriority.application; *)
+     (* let* () = Lwt_unix.sleep 5.0 in *)
+     (* track_info_w#misc#style_context#add_provider (\* Does not cascade! *\) *)
+     (*   (css_provider_from_data "* { background-color: purple }") *)
+     (*   GtkData.StyleContext.ProviderPriority.application; *)
+
+     (* GtkData.Style.set_bg (track_info_w#style) `NORMAL (Gdk.Color.color_parse "#ff00ff"); *)
+     (* let style = track_info_w#misc#style#copy in *)
+     (* style#set_bg [`NORMAL,`NAME "#ff00ff"]; *)
+
+
      let pb_init = GdkPixbuf.create ~width:1 ~height:1 () in
      let img = GMisc.image ~pixbuf:pb_init ~packing:vbox#add () in
+
+     (* TODO:
+        Okay:
+        GdkPixbuf.get_pixels (GdkPixbuf.from_file "./test/data/black_10x10.jpg");;
+        seg fault:
+        GdkPixbuf.get_pixels (GdkPixbuf.from_file "./test/data/ab67616d0000b27338d7a50443e2a6043d6da247.jpg");;
+
+        get_pixels seems to have some issues: https://github.com/bcpierce00/unison/issues/1075
+
+        TODO:
+        let buf =
+          let b = Buffer.create (640*640) in
+          GdkPixbuf.save_to_buffer ~typ:"bmp" (GdkPixbuf.from_file "./test/data/ab67616d0000b27338d7a50443e2a6043d6da247.jpg") b;
+          b;;
+        Thinking of saving in-memory Buffer, then using another Image library to read raw pixels (stb_image, imagelib, Camlimages)
+          Library should be able to read Buffer.t (else would have to read from cache)
+        Gtk seems to support saving as “jpeg”, “png”, “ico” and “bmp”
+      *)
+
+     let colors = [|"#ff0000";"#00ff00";"#0000ff"|] in
 
      let _update_album_art =
        Lwt_react.S.map
@@ -148,10 +234,29 @@ let gui () =
                  let* () =
                    Lwt_result.map_error (fun e -> [ e ]) (ArtUrl.A.to_cache a)
                  in
-                 let pb = GdkPixbuf.from_file @@ ArtUrl.A.abs_path a in
+                 let a_filepath = ArtUrl.A.abs_path a in
+                 (* let a_filepath = "/home/jonat/ocaml/busqer/test/data/black_10x10.jpg" in *)
+                 let centroids = (mat_to_centroids (ArtUrl.(jpg_to_mat a_filepath))) in
+
+                 let pb = GdkPixbuf.from_file a_filepath in
                  Lwt_result.return
                  @@ Lwt_mutex.with_lock mx_gui (fun () ->
-                        Lwt.return @@ img#set_pixbuf pb)))
+                        Lwt.return @@ (img#set_pixbuf pb;
+                                       (* add_provider_to_screen (mk_provider (css_str (pb_to_ran_color pb))) *)
+                                       (* (pb_to_ran_color pb); *)
+                                       (* (ArtUrl.pixbuf_to_array pb) *)
+                                       (* Log.out "arr size: %i" @@ Array.length (ArtUrl.pixbuf_to_array pb) *)
+                                       (* Log.out "dim2: %i" @@ Lacaml.S.Mat.dim2 (ArtUrl.(jpg_to_mat a_filepath)) *)
+                                       (* (mat_to_ran_color (ArtUrl.(jpg_to_mat a_filepath))) *)
+                                       ignore @@ Log.out "num_centoids: %i" @@ List.length @@ centroids;
+                                       (* TODO: centroids can be negative values *)
+                                       let color = (centroids_to_ran_color centroids) in
+                                       ignore @@ Log.out "ran_color: %s" color;
+                                       add_provider_to_screen (mk_provider (css_str color))
+                                       (* Log.out "color: %s" (css_str (rng_elt colors)) *)
+                        (* add_provider_to_screen (mk_provider (css_str (rng_elt colors))); *)
+
+         ))))
          art_url
      in
 
